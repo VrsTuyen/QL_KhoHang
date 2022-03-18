62668d10fb1f22c517da72cb43186874d377e0d9
VrsTuyen <vrstuyen@gmail.com>
2022-03-18 13:19:34 +0700

    cm

--- QL_KhoHang/Program.cs
+++ QL_KhoHang/Program.cs
@@ -17,7 +17,7 @@ namespace QL_KhoHang
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmHoadon());
+            Application.Run(new frmPhieunhap());
         }
     }
 }
